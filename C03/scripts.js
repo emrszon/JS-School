d81f1e6c34c5d8cabf618dcfdcb3b4c3a15fec96
Herrera Diaz <emrahedi@live.com>
2019-09-12 17:50:18 -0500

    updating changes

---
+++ C03/scripts.js
@@ -0,0 +1,57 @@
+      // tippy animation
+      tippy('.books', {
+          arrow: true,
+          touchHold: true,
+          delay: [150, 100],
+          animation: 'scale',
+          html: '#BookInfo'
+
+      })
+
+
+      function openNav() {
+          document.getElementById("mySidenav").style.width = "250px";
+      }
+
+      function closeNav() {
+          document.getElementById("mySidenav").style.width = "0";
+      }
+
+      function showUserOptions() {
+          document.getElementById("userDrop").classList.toggle("show");
+      }
+
+
+      function showMobileOptions() {
+          document.getElementById("mobileDrop").classList.toggle("show");
+      }
+
+
+      window.onclick = function(event) {
+          if (!event.target.matches('.header img')) {
+
+              var dropdowns = document.getElementsByClassName("user-dropdown-content");
+              var i;
+
+              for (i = 0; i < dropdowns.length; i++) {
+                  var openDropdown = dropdowns[i];
+                  if (openDropdown.classList.contains('show')) {
+                      openDropdown.classList.remove('show');
+                  }
+              }
+          }
+
+          if (!event.target.matches('.header i')) {
+
+              var dropdowns = document.getElementsByClassName("mobile-drop-content");
+              var i;
+
+              for (i = 0; i < dropdowns.length; i++) {
+                  var openDropdown = dropdowns[i];
+                  if (openDropdown.classList.contains('show')) {
+                      openDropdown.classList.remove('show');
+                  }
+              }
+          }
+
+      }
