9dda94c165381ceadf0b393a482c75991665ad3a
Herrera Diaz <emrahedi@live.com>
2019-09-19 12:27:21 -0500

    updating changes

--- C04/script.js
+++ C04/script.js
@@ -1,6 +1,6 @@
 // load audio
 var keyid = ["c", "cs", "d", "ds", "e", "f", "fs", "g", "gs", "a", "as", "b", "c", "cs", "d", "ds", "e", "f", "fs", "g", "gs", "a", "as", "b"]
-
+var codekey = [81, 50, 87, 51, 69, 82, 53, 84, 54, 89, 55, 85, 86, 71, 66, 72, 78, 77, 75, 188, 76, 190, 192, 189]
 
 
 
@@ -20,9 +20,9 @@ var blackKeys = document.getElementsByClassName("black");
 for (var i = 0; i < blackKeys.length; i++) {
     blackKeys.item(i).addEventListener("mousedown", function(event) {
         var audio = document.getElementById(this.id + "-sound");
-        audio.currentTime = 0;
-        audio.volume = 1;
         audio.pause();
+        audio.volume = 1;
+        audio.currentTime = 0;
         audio.play();
     });
 }
@@ -38,7 +38,7 @@ for (var i = 0; i < blackKeys.length; i++) {
             if (volume === 0) {
                 clearInterval(fadeVolume);
             }
-        }, 25);
+        }, 23);
     });
 }
 
@@ -46,11 +46,10 @@ var whiteKeys = document.getElementsByClassName("white");
 for (var i = 0; i < whiteKeys.length; i++) {
     whiteKeys.item(i).addEventListener("mousedown", function(event) {
         var audio = document.getElementById(this.id + "-sound");
+        audio.pause();
         audio.currentTime = 0;
         audio.volume = 1;
-        audio.pause();
         audio.play();
-
     });
 }
 
@@ -65,7 +64,55 @@ for (var i = 0; i < whiteKeys.length; i++) {
             audio.volume = volume / 100;
             if (volume === 0) {
                 clearInterval(fadeVolume);
+                //audio.pause();
             }
-        }, 25);
+        }, 23);
     });
+}
+var pianoKeys = document.getElementsByTagName('li');
+var pianoKeysArr = Array.from(pianoKeys);
+
+document.addEventListener("keydown", function myFunction(event) {
+    var x = event.which || event.codekey;
+    if (keyindex(x) !== -1) {
+        var audio = document.getElementById(keyindex(x) + keyid[keyindex(x)] + "-sound");
+        audio.pause();
+        audio.currentTime = 0;
+        audio.volume = 1;
+        audio.play();
+    }
+});
+
+// document.addEventListener("keyup", function myFunction(event) {
+//     var x = event.which || event.codekey;
+//     console.log(x);
+
+// });
+
+// document.addEventListener("keyup", function myFunction(event) {
+//     var x = event.which || event.codekey;
+//     console.log(keyindex(x));
+//     var audio = document.getElementById(keyindex(x) + keyid[keyindex(x)] + "-sound");
+
+//     var volume = 100;
+//     var fadeVolume = setInterval(function() {
+//         volume -= 10;
+//         audio.volume = volume / 100;
+//         if (volume === 0) {
+//             clearInterval(fadeVolume);
+//             //audio.pause();
+//         }
+//     }, 23);
+
+// });
+
+function keyindex(x) {
+    for (var i = 0; i < codekey.length; i++) {
+        if (codekey[i] == x) {
+            return i;
+        }
+
+    }
+
+    return -1;
 }
